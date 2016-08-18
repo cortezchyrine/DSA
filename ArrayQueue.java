804c85e19ee5580fca72110cd60e0cc1ff13ed3e
cortezchyrine <zhynli30@gmail.com>
2016-08-18 12:44:49 +0800

    Update ArrayQueue.java

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -1,6 +1,14 @@
 package queue;
 
+/*
+SOURCE CODE: ArrayQueue.java
+AUTHOR NAME: Cortez, Chyrine A.
+GITHUB URL: github.com/cortezchyrine
+LAST UPDATE: [08/14/2016]
+
+*/
 import java.util.*;
+
 public class ArrayQueue {
 	public static void main(String[] args) {
 		int a=0;
@@ -9,16 +17,16 @@ public class ArrayQueue {
 		
 			
 		Queue2 arque = new Queue2(10);
-		System.out.println("^*^*^*^*^*^*^*^*^*^*^*^*\n");
-		System.out.println("Current Queue \n");
-		arque.showQueue();
+			System.out.println("^*^*^*^*^*^*^*^*^*^*^*^*\n");
+			System.out.println("Current Queue \n");
+			arque.showQueue();
 		
 				
 		System.out.println("^*^*^*^*^*^*^*^*^*^*^*^*\n");
-		System.out.print("How many would you like to Enqueue? : ");
-		a = input.nextInt();
-		enq	= new int[a];
-		System.out.println("Enqueuing\n");
+			System.out.print("How many would you like to Enqueue? : ");
+			a = input.nextInt();
+			enq	= new int[a];
+			System.out.println("Enqueuing\n");
 		for (int i=0;i<=a;i++){
 			arque.enqueue(i);	
 		}
@@ -32,9 +40,8 @@ public class ArrayQueue {
 		arque.peekRear();
 	
 		System.out.println("Dequeuing\n");
-		for (int i=0;i<a;i++){
-			arque.dequeue(i);	
-		}
-	}
-	
+			for (int i=0;i<a;i++){
+				arque.dequeue(i);	
+			}
+	}	
 }
